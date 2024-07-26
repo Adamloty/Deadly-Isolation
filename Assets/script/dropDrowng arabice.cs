35771a607e9e9b8311dce22349e5607b8af18c41
Adamlotfy <161876835+Adamloty@users.noreply.github.com>
2024-07-27 00:56:43 +0300

    ui

---
+++ Assets/script/dropDrowng arabice.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEditor.Experimental.GraphView;
+
+
+public class dropDrowngarabice : MonoBehaviour
+{
+    [SerializeField] private TMP_Dropdown lang;
+    public GameObject Arb;
+    public GameObject English;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (lang.value == 0)
+        {
+            Arb.SetActive(false);
+           English.SetActive(true);
+
+
+
+
+        }
+        else if (lang.value == 1)
+        {
+            Arb.SetActive(true);
+            English.SetActive(false);
+
+        }
+    }
+}
