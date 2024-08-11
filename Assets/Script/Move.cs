8f5bf5eca60916c30df3f7725b3574d8ea367081
Adamlotfy <161876835+Adamloty@users.noreply.github.com>
2024-08-11 15:46:34 +0300

    تحديث في الحركة

--- Assets/Script/Move.cs
+++ Assets/Script/Move.cs
@@ -1,28 +1,37 @@
-using Photon.Pun;
+﻿using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using Photon.Realtime;
+using Photon.Voice.PUN;
 using UnityEngine.SceneManagement;
+using Photon.Voice.Unity;
 //using System.Security.Cryptography;
 public class move : MonoBehaviourPunCallbacks
 {
-    private float Gravity = 20f;
-    [SerializeField] private float jump = 20f;
+    //private float Gravity = 20f;
+    [SerializeField] private float jump = 10000f;
     public float speed = 1f;
     Vector3 moveDirection = Vector3.zero;
-    private CharacterController ch;
+   // private CharacterController ch;
     private PhotonView pv;
     private Animator anim;
     [SerializeField] private TextMeshPro Username;
-
+    private bool shiftdown;
+    private bool reconnecting = false;
+    private VoiceConnection voiceConnection;
+    private Rigidbody rb;
+    private bool isGrounded;
+    public float gravityMultiplier = 0.5f;
+    public float minFallSpeed = -5f; // الحد الأدنى للسرعة أثناء النزول
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
-        ch = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+        //ch = GetComponent<CharacterController>();
         pv = GetComponent<PhotonView>();
         if (pv.IsMine)
         {
@@ -35,11 +44,26 @@ public class move : MonoBehaviourPunCallbacks
             Destroy(transform.GetChild(21).gameObject);
            
         }
-   
-       
+        voiceConnection = FindObjectOfType<VoiceConnection>();
+        if (voiceConnection == null)
+        {
+            Debug.LogError("VoiceConnection component not found. Please add it to your scene.");
+        }
+
     }
 
     // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (!isGrounded)
+        {
+            rb.AddForce(Physics.gravity * (gravityMultiplier - 1) * rb.mass);
+        }
+
+        // التحقق من إذا كان الكائن على الأرض باستخدام Raycast
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 230f); // 
+
+    }
     void Update()
     {
     
@@ -49,23 +73,43 @@ public class move : MonoBehaviourPunCallbacks
             PhotonNetwork.NickName = PlayerPrefs.GetString("ur");
             pv.RPC("TakeName", RpcTarget.All, PlayerPrefs.GetString("ur"));
             Username.gameObject.SetActive(false);
-            if (ch.isGrounded)
-            {
+            //if (ch.isGrounded)
+         //   {
                 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-                moveDirection *= speed;
                 moveDirection = transform.TransformDirection(moveDirection);
+                if(Input.GetKeyDown(KeyCode.LeftControl)||Input.GetKeyDown(KeyCode.LeftAlt))
+                {
+                    shiftdown = !shiftdown;
+                    if(!shiftdown)
+                    {
+                    //rb.velocity = new Vector3(0, 1, 0);
+                    //transform.Translate(0, jump-50, 0);
+                    rb.AddForce(Vector3.up * (jump-50), ForceMode.Impulse);
+                }
+                }
+                if(shiftdown)
+                {
+                    anim.SetBool("crouch", true);
+                    moveDirection *= speed-50f;
+                }
+                else
+                {
+                    anim.SetBool("crouch", false);
+                    moveDirection *= speed;
+                }
                 if (Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
                 {
                    // anim.SetBool("", false);
-                    if (speed == 1 && !Input.GetKey(KeyCode.LeftShift))
+                    if (speed == 200 && !Input.GetKey(KeyCode.LeftShift))
                     {
                         anim.SetBool("Walk", true);
                         anim.SetBool("Run", false);
                     }
-                    if (speed == 2)
+                    if (speed == 300)
                     {
                         anim.SetBool("Walk", false);
                         anim.SetBool("Run", true);
+                        shiftdown = false;
                     }
 
 
@@ -78,20 +122,30 @@ public class move : MonoBehaviourPunCallbacks
                 if (Input.GetButtonDown("Jump"))
                 {
                     anim.SetTrigger("Jump");
-                    moveDirection.y = jump;
+                    rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
+                     // transform.Translate(0, jump, 0);
                 }
                 if (Input.GetKeyDown(KeyCode.LeftShift))
                 {
-                    speed = 2f;
+                    speed = 600f;
                 }
                 if (Input.GetKeyUp(KeyCode.LeftShift))
                 {
-                    speed = 1f;
+                    speed = 400f;
                 }
+            rb.velocity = new Vector3(moveDirection.x, rb.velocity.y, moveDirection.z);
+
+            if (rb.velocity.y > minFallSpeed)
+            {
+                rb.velocity = new Vector3(rb.velocity.x, minFallSpeed, rb.velocity.z);
+                print("yes");
             }
-            moveDirection.y -= Gravity * Time.deltaTime;
-            ch.Move(moveDirection * Time.deltaTime);
         }
+       
+        // transform.Translate(moveDirection);
+        //  moveDirection.y -= Gravity * Time.deltaTime;
+        //    ch.Move(moveDirection * Time.deltaTime);
+        // }
     }
 
     [PunRPC]
@@ -141,66 +195,79 @@ public class move : MonoBehaviourPunCallbacks
             //}
       //  }
     }
-    // [PunRPC]
+
+ 
+  
+
+    // سيتم استدعاء هذا الحدث عند انقطاع الاتصال
     public override void OnDisconnected(DisconnectCause cause)
     {
-        PhotonNetwork.LoadLevel(1);
+        Debug.LogWarningFormat("OnDisconnected() was called by PUN with reason {0}", cause);
+
+        if (!reconnecting)
+        {
+            reconnecting = true;
+            Invoke("Reconnect", 5); // الانتظار لمدة 5 ثوانٍ قبل إعادة المحاولة
+        }
     }
-    public override void OnLeftRoom()
+
+    // محاولة إعادة الاتصال
+    private void Reconnect()
     {
-        PhotonNetwork.LoadLevel(1);
+        if (PhotonNetwork.ReconnectAndRejoin())
+        {
+            Debug.Log("Reconnecting...");
+        }
+        else
+        {
+            Debug.LogError("Reconnect and rejoin failed. Retrying...");
+            Invoke("Reconnect", 5); // إعادة المحاولة بعد 5 ثوانٍ أخرى
+        }
     }
 
-    //private void Animation()
-    //{
-    //    if(Input.GetAxisRaw("Vertical")!=0||Input.GetAxisRaw("Horizontal")!=0)
-    //    {
-    //        if(speed==6&&!Input.GetKey(KeyCode.LeftShift))
-    //        {
-    //            anim.SetBool("Walk", true);
-    //            anim.SetBool("Run", false);
-    //        }
-    //        if (speed == 12)
-    //        {
-    //            anim.SetBool("Walk", false);
-    //            anim.SetBool("Run", true);
-    //        }
+    // سيتم استدعاء هذا الحدث عند النجاح في إعادة الانضمام للغرفة
+    public override void OnJoinedRoom()
+    {
+        reconnecting = false;
+        Debug.Log("Reconnected and rejoined the room successfully.");
 
+        // إعادة الاتصال بـ Photon Voice
+        ReconnectVoice();
+    }
 
-    //    }
-    //    else if (Input.GetAxisRaw("Vertical") == 0 && Input.GetAxisRaw("Horizontal") == 0)
-    //    {
-    //        anim.SetBool("Walk", false);
-    //        anim.SetBool("Run", false);
-    //    }
-    //    if(transform.position.y<-10)
-    //    {
-    //        anim.SetBool("Walk", false);
-    //        anim.SetBool("Run", false);
-    //    }
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.LogErrorFormat("OnJoinRoomFailed() was called by PUN with error code {0} and message {1}", returnCode, message);
+        Invoke("Reconnect", 5); // إعادة المحاولة بعد 5 ثوانٍ أخرى
+    }
 
-    //}
-    //private void Awake()
-    //{
-    //   // if (pv.IsMine)
-    //  //  {
-    //    try
-    //    {
-    //        if (PlayerLeftanim == null)
-    //        {
-    //            PlayerLeftanim = GameObject.FindWithTag("PlayerLeft").GetComponent<Animator>();
-    //        }
-    //        if (PlayerLeftanim != null && PlayerLeftanim == null)
-    //        {
-    //            playerleftname = PlayerLeftanim.gameObject.GetComponentInChildren<Text>();
-    //        }
-    //    }
-    //    catch
-    //    {
-    //        print("Nothing");
-    //    }
+    // إعادة الاتصال بـ Photon Voice
+    private void ReconnectVoice()
+    {
+        if (voiceConnection != null)
+        {
+            if (voiceConnection.ClientState == Photon.Realtime.ClientState.Joined)
+            {
+                Debug.Log("Photon Voice is already connected.");
+            }
+            else
+            {
+                Debug.Log("Reconnecting Photon Voice...");
+                voiceConnection.ConnectUsingSettings();
+            }
+        }
+    }
 
-    //   // }
+    // سيتم استدعاء هذا الحدث عند النجاح في الاتصال بـ Photon Voice
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Photon Voice reconnected successfully.");
+    }
 
-    //}
+    // التعامل مع أخطاء الاتصال بـ Photon Voice
+    private void OnVoiceDisconnected(DisconnectCause cause)
+    {
+        Debug.LogErrorFormat("Failed to reconnect Photon Voice with reason {0}. Retrying...", cause);
+        Invoke("ReconnectVoice", 5); // إعادة المحاولة بعد 5 ثوانٍ أخرى
+    }
 }
---
+++ Assets/test/Scipts/ProceduralMesh.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ProceduralMesh : MonoBehaviour
+{
+    public int pointsPerChunk = 100; // عدد النقاط لكل Chunk
+    public int chunkSize = 16; // حجم الـ Chunk
+    public float maxHeight = 5f; // أقصى ارتفاع للنقاط
+    public Vector2Int chunkCoordinate; // إحداثيات الـع Chunk
+
+    private Mesh mesh;
+    private List<Vector3> vertices;
+    private List<int> triangles;
+
+    public void Initialize(Vector2Int coord)
+    {
+        chunkCoordinate = coord;
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        GenerateMesh();
+    }
+
+    void GenerateMesh()
+    {
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+
+        // توليد النقاط بشكل عشوائي
+        for (int i = 0; i < pointsPerChunk; i++)
+        {
+            float x = Random.Range(0, chunkSize);
+            float z = Random.Range(0, chunkSize);
+            float y = Random.Range(0, maxHeight);
+
+            vertices.Add(new Vector3(x, y, z));
+        }
+
+        // توليد المثلثات
+        for (int i = 0; i < pointsPerChunk - 2; i += 3)
+        {
+            triangles.Add(i);
+            triangles.Add(i + 1);
+            triangles.Add(i + 2);
+        }
+
+        // تطبيق النقاط والمثلثات على الـ Mesh
+        mesh.Clear();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals(); // إعادة حساب الاتجاهات العادية للحصول على إضاءة صحيحة
+    }
+
+    void OnDrawGizmos()
+    {
+        if (vertices == null) return;
+
+        Gizmos.color = Color.red;
+        foreach (var vertex in vertices)
+        {
+            Gizmos.DrawSphere(transform.position + vertex, 0.1f);
+        }
+    }
+}
